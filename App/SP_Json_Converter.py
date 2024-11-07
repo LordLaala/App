e64bd124131bf23f1497fe058007d0a9e1913559
LordLaala <144143021+LordLaala@users.noreply.github.com>
2024-11-07 16:06:43 +0100

    Update SP_Json_Converter.py

--- App/SP_Json_Converter.py
+++ App/SP_Json_Converter.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from DocConverter import DocConverter
 from JsonConverter import JsonConverter
 import Checker as check
@@ -14,45 +13,40 @@ def convert_to_json(filepath, filename, root, output_dir):
     except UnicodeError:
         document.save_data_to_json(os.path.join(output_dir, json_filename))
 
-def process_file(root, filename, doc, log, output_dir, count):
-    if filename.startswith(("A_", "a_")):
-        if not filename.endswith((".docx", ".DOCX")):
-            filepath = os.path.join(root, filename)
+def process_file(entry, root, doc, log, output_dir, count):
+    if entry.name.startswith(("A_", "a_")):
+        if not entry.name.endswith((".docx", ".DOCX")):
+            filepath = os.path.join(root, entry.name)
             try:
                 count += 1
                 root_name = root.split("\\")[6]
-                filepath = doc.convert_doc_to_docx_sp(filepath, f'{count}_{root_name}_{filename}.docx')
+                filepath = doc.convert_doc_to_docx_sp(filepath, f'{count}_{root_name}_{entry.name}.docx')
             except Exception as e:
-                log.write_error_messages(root, filename, e, "DocConverter")
+                log.write_error_messages(root, entry.name, e, "DocConverter")
                 return
-            if check.search_word_in_docx(filepath, "Stammdatei", filename, root):
-                convert_to_json(filepath, f'{count}_{root_name}_{filename}', root_name, output_dir)
+            if check.search_word_in_docx(filepath, "Stammdatei", entry.name, root):
+                convert_to_json(filepath, f'{count}_{root_name}_{entry.name}', root_name, output_dir)
         else:
-            if check.search_word_in_docx(filepath, "Stammdatei", filename, root):
+            if check.search_word_in_docx(filepath, "Stammdatei", entry.name, root):
                 root_name = root.split("\\")[6]
-                convert_to_json(filepath, f'{count}_{root_name}_{filename}', root, output_dir)
+                convert_to_json(filepath, f'{count}_{root_name}_{entry.name}', root, output_dir)
                 count += 1
 
-def file_generator(directory):
+def scan_directory(directory, doc, log, output_dir):
+    count = 0
     for root, dirs, files in os.walk(directory):
         if "\\NHF" in root or "\\SF" in root:
-            for filename in files:
-                yield root, filename
+            with os.scandir(root) as it:
+                for entry in it:
+                    if entry.is_file():
+                        process_file(entry, root, doc, log, output_dir, count)
 
 def main(directory="T:\\ima\\Alle\\Temp\\SCN\\_05_Grundstücke", output_dir="JSON"):
     log = Logger()
     doc = DocConverter()
     directory = Path(directory)
     os.makedirs(output_dir, exist_ok=True)
-    count = 0
-
-    with ThreadPoolExecutor() as executor:
-        futures = []
-        for root, filename in file_generator(directory):
-            futures.append(executor.submit(process_file, root, filename, doc, log, output_dir, count))
-
-        for future in as_completed(futures):
-            future.result()
+    scan_directory(directory, doc, log, output_dir)
 
 if __name__ == "__main__":
     main()
